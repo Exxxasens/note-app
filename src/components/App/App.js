d9e8028804f7f48aa05ec15f18ea1bfca61b37f0
Oleg Okatev <oleg-okatev@hotmail.com>
2020-10-03 17:21:00 +0300

    App component refactoring, mock data

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,27 +1,51 @@
 import React from 'react';
 import './App.scss';
 import Menu from '../Menu';
-import TopBar from '../TopBar';
-import {
-    HashRouter as Router,
-    Switch,
-    Route
-  } from "react-router-dom";
+import Layout from '../Layout';
+import NoteList from '../NoteList';
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+
+
+const notes = [
+    {
+        title: 'Сделать приложение',
+        isImportant: true,
+        category: null,
+        color: 'blue',
+        isSync: false,
+        complete: false,
+        createdAt: new Date(),
+        updatedAt: new Date()
+    },
+    {
+        title: 'Выкинуть Андрея',
+        isImportant: false,
+        category: 'Работа',
+        color: 'red',
+        isSync: false,
+        complete: true,
+        createdAt: new Date(),
+        updatedAt: new Date()
+    }
+]
 
 export default () => {
+
+    const [data, setData] = React.useState({
+        notes
+    });
+
     return (
         <Router>
             <div className='wrapper'>
                 <Menu/>
-                <div className='content'>
-                    <Switch>
-                        <Route path='/create' >
-                            <TopBar>
-                                <h1>Создайте первую заметку!</h1>
-                            </TopBar>
-                        </Route>
-                    </Switch>
-                </div>
+                <Switch>
+                    <Route path='/list' >
+                        <Layout topBar={<h2>Мои напоминания</h2>}>
+                            <NoteList list={notes}/>
+                        </Layout>
+                    </Route>
+                </Switch>
             </div>
         </Router>
     )
