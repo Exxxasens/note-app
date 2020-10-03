4e4bc5d544afe0fa6ee1e35757286c34a1cfe641
Oleg Okatev <oleg-okatev@hotmail.com>
2020-10-03 23:05:57 +0300

    useTouchBar hook

---
+++ src/components/hooks/useTouchBar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+const { TouchBar, getCurrentWindow } = require('electron').remote;
+
+export default (createTouchBarFn, touchBarUpdateFn) => {
+    let touchBarItems = React.useMemo(() => {
+        const items = createTouchBarFn(TouchBar);
+        getCurrentWindow().setTouchBar(new TouchBar({ items }));
+        return items;
+
+    }, [createTouchBarFn]);
+
+    React.useEffect(() => {
+        if(touchBarItems) touchBarUpdateFn(touchBarItems);
+    }, [touchBarUpdateFn]);
+
+    React.useEffect(() => {
+        return () => getCurrentWindow().setTouchBar(null);
+    }, []);
+
+}
