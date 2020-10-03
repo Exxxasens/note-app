97f2179ef155370d0e937ad825910fc4f470620e
Oleg Okatev <oleg-okatev@hotmail.com>
2020-10-03 17:23:03 +0300

    Menu style and component refactoring

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -2,13 +2,33 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './Menu.scss';
 
+const menu = [
+    { 
+        label: 'Напоминания', icon: 'list', link: '/list/',
+        submenu: [
+            { label: 'Все', link:'all' },
+            { label: 'Сегодня', link: 'today' },
+            { label: 'Завтра', link: 'tomorrow' },
+            { label: 'Эта неделя', link: 'week' }
+        ]
+    },
+    { label: 'Календарь', icon: 'calendar_today', link: '/calendar' },
+    { label: 'Настройки', icon: 'settings', link: '/settings' }
+];
+
 export default () => {
-    const list = [
-        { label: 'Добавить', icon: 'add', link: '/create' },
-        { label: 'Календарь', icon: 'calendar_today', link: '/calendar' },
-        { label: 'Настройки', icon: 'settings', link: '/settings' }
-    ];
-    const mapFn = ({ label, icon, link }, i) => <MenuItem label={label} icon={icon} link={link} key={i} showLabel={false} />
+    const [submenu, setSubmenu] = React.useState(null);
+    const onSelect = (submenu) => setSubmenu(submenu); 
+    const mapFn = ({ label, icon, link, submenu }, i) => (<MenuItem
+        label={label}
+        icon={icon}
+        link={link}
+        submenu={submenu}
+        key={i}
+        showLabel={false}
+        handleClick={onSelect}
+    />)
+
     return (
         <div className='menu-wrapper'>
             <div>
@@ -16,18 +36,34 @@ export default () => {
             </div>
             <div className='menu-row'>
                 <div className='menu'>
-                    { list.map(mapFn) }
+                    { menu.map(mapFn) }
                 </div>
+                { submenu ? <SubMenu list={submenu}/> : null }
             </div>
         </div>
     )
 }
 
-const MenuItem = ({ label, icon, link, showLabel = true, showIcon = true }) => {
+const MenuItem = ({ label, icon, link, showLabel = true, showIcon = true, handleClick, submenu }) => {
     return (
-        <NavLink to={link} className='menu-item' activeClassName='selected' >
+        <NavLink to={link} className='menu-item' activeClassName='selected' onClick={() => handleClick(submenu)} >
                 { icon && showIcon ? (<div className='icon'><span className="material-icons">{icon}</span></div>) : null }
                 { label && showLabel ? <div>{ label }</div> : null }
         </NavLink>
+    )
+}
+
+const SubMenu = ({ list }) => {
+    const mapFn = ({ label, link }, i) => {
+        return (
+            <NavLink to={link} className='submenu-item' activeClassName='selected' key={i}>
+                { label }
+            </NavLink>
+        )
+    }
+    return (
+        <div className='right-side-menu'>
+            { list.map(mapFn) }
+        </div>
     )
 }
