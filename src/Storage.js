cc8d69db782241b4829449d503da7b0c2697a27f
Oleg Okatev <oleg-okatev@hotmail.com>
2020-10-10 19:56:36 +0300

    nedb StorageApi

--- src/Storage.js
+++ src/Storage.js
@@ -1,73 +1,73 @@
-class EventEmitter {
-    constructor() {
-        this.events = {};
-    }
-
-    emit(name, ...args) {
-    		if(!this.events[name]) return;
-        this.events[name].map(fn => fn(...args));
-        return this;
-    }
-
-    on(name, handler) {
-        if(!this.events[name]) this.events[name] = [];
-        this.events[name].push(handler);
-        return handler;
-    }
-
-    removeListener(name, handler) {
-        this.events[name] = this.events[name].filter(fn => fn !== handler);
-        
-        return this;
-    }
-		
-    once(name, handler) {
-        let wrapper = (...args) => {
-        		handler(...args);
-            this.removeListener(name, wrapper);
-        }
-        this.on(name, wrapper);
-        return handler;
-        
-    }
-
+const Database = require('nedb')
+const path = require('path');
+const { app } = require('electron').remote;
+const db = {
+    notes: new Database({ filename: path.join(app.getPath('userData'), 'notes.db'), autoload: true }),
+    catergories: new Database({ filename: path.join(app.getPath('userData'), 'categories.db'), autoload: true })
 }
 
-
-
-class Storage extends EventEmitter {
-    constructor() {
-        super();
+class StorageApi {
+    getAllNotes() {
+        return new Promise((resolve, reject) => {
+            db.notes.find({}, (err, doc) => {
+                if(err) reject(err);
+                resolve(doc);
+            });
+        });
     }
-    get(key) {
-        if(!this.has(key)) throw new Error('Item doesn\'t exist');
-        return JSON.parse(localStorage.getItem(key));
+    getNoteById(id) {
+        return new Promise((resolve, reject) => {
+            db.notes.findOne({ id }, (err, doc) => {
+                if(err) reject(err);
+                resolve(doc);
+            })
+        });
     }
-    set(key, value) {
-        localStorage.setItem(key, JSON.stringify(value));
-        this.emit('change');
-        return value;
+    addNote(note) {
+        return new Promise((resolve, reject) => {
+            db.notes.insert({ ...note, createdAt: Date.now(), updatedAt: Date.now() }, (err, doc) => {
+                if(err) reject(err);
+                resolve(doc);
+            });
+        })
     }
-    has(key) {
-        return Boolean(localStorage.getItem(key));
+    getAllNotesByCategory(name) {
+        return new Promise((resolve, reject) => {
+            db.notes.find({ 'category.name': name }, (err, doc) => {
+                if(err) reject(err);
+                resolve(doc);
+            })
+        })
     }
-    addItem(key, value) {
-        if(this.has(key)) throw new Error('Item is already exist');
-        return this.set(key, value);
+    getAllCategories() {
+        return new Promise((resolve, reject) => {
+            db.catergories.find({}, (err, doc) => {
+                if(err) reject(err);
+                resolve(doc);
+            });
+        });
     }
-    updateItem(key, options) {
-        if(!this.has(key)) throw new Error('Item doesn\'t exist');
-        let item = this.get(key);
-        item = Object.assign(item, options);
-        this.set(key, item);
-        this.emit('change');
-        return item;
+    deleteNote(_id) {
+        return new Promise((resolve, reject) => {
+            db.notes.remove({ _id }, { multi: false }, (err, num) => {
+                if(err) reject(err);
+                resolve(num);
+            });
+        })
     }
-    deleteItem(key) {
-        let item = localStorage.get(key);
-        localStorage.removeItem(key)
-        this.emit('change');
-        return item;
+    updateNote(_id, update) {
+        return new Promise((resolve, reject) => {
+            db.notes.update({ _id }, { $set: update }, {}, (err, num) => {
+                if(err) reject(err);
+                resolve(num);
+            });
+        });
+    }
+    clearAll() {
+        return new Promise((resolve, reject) => 
+            db.notes.remove({}, { multi: true }, (err, num) => err ? reject(err) : resolve(num))
+        );
     }
 }
 
+export default StorageApi;
